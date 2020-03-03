c61bb76d45267360e2416ea65429340f82d009ee
cccclf <774852667@qq.com>
2020-03-03 13:57:53 +0800

    add publishcontroller

---
+++ src/main/java/life/chen/community/controller/PublishController.java
@@ -0,0 +1,92 @@
+package life.chen.community.controller;
+
+import life.chen.community.mapper.QuestionMapper;
+import life.chen.community.mapper.UserMapper;
+import life.chen.community.model.Question;
+import life.chen.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class PublishController {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/publish")
+    public String publish() {
+        return "publish";
+    }
+
+
+    //提交掉表单后找到地址为"/publish"的，并且请求方法为PostMapping
+    @PostMapping("/publish")
+    public String doPublish(
+            //21讲这里有变化
+            //通过RequestParam去接收三个参数，
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            HttpServletRequest request,
+            Model model) {
+        //同时首先放入model里面，为了能够回显到页面上去
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
+
+        //其次，验证参数是否为空，空的话提示信息，需要补充
+        if (title == null || title == "") {
+            model.addAttribute("error", "标题不能为空");
+            return "publish";
+        }
+        if (description == null || description == "") {
+            model.addAttribute("error", "问题补充不能为空");
+            return "publish";
+        }
+        if (tag == null || tag == "") {
+            model.addAttribute("error", "标签不能为空");
+            return "publish";
+        }
+
+
+        //若参数参数内容不为空，则验证用户是否登录，用index的方法去实现
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+        //如果用户未登录，则返回错误信息到前面
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
+
+        //一切正常后，构建对象，插入数据库，实现功能
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+        questionMapper.create(question);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/life/chen/community/mapper/QuestionMapper.java
@@ -0,0 +1,16 @@
+package life.chen.community.mapper;
+/*
+2020-3-2学习：
+完成发布文章的功能
+ */
+
+import life.chen.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    void create(Question question);
+
+}
