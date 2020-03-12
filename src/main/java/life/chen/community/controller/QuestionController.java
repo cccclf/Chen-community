3515b03c24b8d6c5af241fa11f3fedbec16ac876
cccclf <774852667@qq.com>
2020-03-12 17:28:19 +0800

    finish 'my problem' page

---
+++ src/main/java/life/chen/community/controller/QuestionController.java
@@ -0,0 +1,26 @@
+package life.chen.community.controller;
+
+import life.chen.community.dto.QuestionDTO;
+import life.chen.community.mapper.QuestionMapper;
+import life.chen.community.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class QuestionController {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("/question/{id}")
+    //拿到id去数据库中查询是否存在
+    public String question(@PathVariable(name = "id") Integer id,
+                           Model model) {
+        QuestionDTO questionDTO = questionService.getById(id);
+        model.addAttribute("question", questionDTO);
+        return "question";
+    }
+}
