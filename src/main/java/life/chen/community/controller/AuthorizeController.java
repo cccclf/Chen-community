62b0c1d4714f39b6d2e44ca331188cff8ec86e36
cccclf <774852667@qq.com>
2020-03-13 15:47:49 +0800

    修复登录的问题

--- src/main/java/life/chen/community/controller/AuthorizeController.java
+++ src/main/java/life/chen/community/controller/AuthorizeController.java
@@ -5,12 +5,14 @@ import life.chen.community.dto.GithubUser;
 import life.chen.community.mapper.UserMapper;
 import life.chen.community.model.User;
 import life.chen.community.provider.GithubProvider;
+import life.chen.community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
@@ -28,7 +30,7 @@ public class AuthorizeController {
     private GithubProvider githubProvider;
 
     @Autowired
-    private UserMapper userMapper;
+    private UserService userService;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
@@ -49,10 +51,8 @@ public class AuthorizeController {
             user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
-            user.setGmtCreate(System.currentTimeMillis());
-            user.setGmtModified(user.getGmtCreate());
             user.setAvatarUrl(githubUser.getAvatar_url());
-            userMapper.insert(user);
+            userService.createOrUpdate(user);
             //并且把token放到cookie里面
             response.addCookie(new Cookie("token",token));
             return "redirect:/";
@@ -60,5 +60,15 @@ public class AuthorizeController {
             //登录失败，重新登录
             return "redirect:/";
         }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request,
+                         HttpServletResponse response){
+        request.getSession().removeAttribute("user");
+        Cookie cookie = new Cookie("token",null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "redirect:/";
     }
 }
