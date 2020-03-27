810ec77771df126d5b767b8e2fc7f673bd24f7f9
cccclf <774852667@qq.com>
2020-03-27 16:47:27 +0800

    实现了'回复列表'的功能

--- src/main/java/life/chen/community/service/CommentService.java
+++ src/main/java/life/chen/community/service/CommentService.java
@@ -1,16 +1,24 @@
 package life.chen.community.service;
 
+import life.chen.community.dto.CommentDTO;
 import life.chen.community.enums.CommentTypeEnum;
 import life.chen.community.exception.CustomizeErrorCode;
 import life.chen.community.exception.CustomizeException;
 import life.chen.community.mapper.CommentMapper;
 import life.chen.community.mapper.QuestionExtMapper;
 import life.chen.community.mapper.QuestionMapper;
-import life.chen.community.model.Comment;
-import life.chen.community.model.Question;
+import life.chen.community.mapper.UserMapper;
+import life.chen.community.model.*;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -23,6 +31,9 @@ public class CommentService {
 
     @Autowired
     private QuestionExtMapper questionExtMapper;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Transactional
     public void insert(Comment comment) {
@@ -42,12 +53,45 @@ public class CommentService {
         } else {
             //回复问题
             Question question = questionMapper.selectByPrimaryKey(comment.getParentId());
-            if (question == null){
-                throw  new CustomizeException(CustomizeErrorCode.QUESTION_NOT_FOUND);
+            if (question == null) {
+                throw new CustomizeException(CustomizeErrorCode.QUESTION_NOT_FOUND);
             }
             commentMapper.insert(comment);
             question.setCommentCount(1);
             questionExtMapper.incCommentCount(question);
         }
+    }
+
+    public List<CommentDTO> listByQuestionId(Long id) {
+        CommentExample commentExample = new CommentExample();
+        commentExample.createCriteria()
+                .andParentIdEqualTo(id)
+                .andTypeEqualTo(CommentTypeEnum.QUESTION.getType());
+        List<Comment> comments = commentMapper.selectByExample(commentExample);
+
+        if (comments.size() == 0) {
+            return new ArrayList<>();
+        }
+        //获取去重的评论人
+        Set<Long> commentators = comments.stream().map(comment -> comment.getCommentator()).collect(Collectors.toSet());
+        List<Long> userIds = new ArrayList<>();
+        userIds.addAll(commentators);
+
+        //获取评论人并转换为map
+        UserExample userExample = new UserExample();
+        userExample.createCriteria()
+                .andIdIn(userIds);
+        List<User> users = userMapper.selectByExample(userExample);
+        Map<Long, User> userMap = users.stream().collect(Collectors.toMap(user -> user.getId(), user -> user));
+
+        //转换comment为commentDTO
+        List<CommentDTO> commentDTOS = comments.stream().map(comment -> {
+            CommentDTO commentDTO = new CommentDTO();
+            BeanUtils.copyProperties(comment, commentDTO);
+            commentDTO.setUser(userMap.get(comment.getCommentator()));
+            return commentDTO;
+        }).collect(Collectors.toList());
+
+        return commentDTOS;
     }
 }
